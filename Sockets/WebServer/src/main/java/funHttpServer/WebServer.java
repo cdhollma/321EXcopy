a2f095437dc1651057feb0e014975e1bb8d7de4b
cdhollma <cdhollma@asu.edu>
2025-01-27 14:38:14 -0800

    Update WebServer.java

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -203,17 +203,18 @@ class WebServer {
 
           // extract required fields from parameters
           Integer num1 = 10;
- 	       Integer num2 = 10;
+ 	  Integer num2 = 10;
           
 	       try{
-	  	       num1 = Integer.parseInt(query_pairs.get("num1"));
-             num2 = Integer.parseInt(query_pairs.get("num2"));
+	         num1 = Integer.parseInt(query_pairs.get("num1"));
+             	 num2 = Integer.parseInt(query_pairs.get("num2"));
 	      }
 	      catch(Exception e){
 	  	      builder.append("HTTP/1.1 406 Not Acceptable\n");
 	 	      builder.append("Content-Type: text/html; charset=utf-8\n");
 		      builder.append("\n");
 		      builder.append("Must supply integer or non-null arguments for multiply");
+		      break;
 	     }
           // do math
           Integer result = num1 * num2;
